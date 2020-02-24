11e316693186bbdc105ec751b1dcf12c7611ea8a
WickWen <mrwickweb@163.com>
2020-02-25 00:50:42 +0800

    安装并引入 axios 和Element-ui

--- src/main.js
+++ src/main.js
@@ -2,6 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// axios 
+import axios from "axios";
+Vue.prototype.$axios = axios;
+axios.defaults.baseURL = "http://liangwei.tech:3000"
+
+// ElementUI
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
+
+
+
+
+
 Vue.config.productionTip = false
 
 new Vue({
