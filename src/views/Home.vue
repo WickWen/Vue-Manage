abe12d92ac7454c64c1bbce590a87569d832d93e
WickWen <mrwickweb@163.com>
2020-02-27 00:45:00 +0800

    使用容器组件搭建页面的基本结构

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,11 @@
 <template>
-  <el-row type="flex" justify='center' align='middle' class="wrapper"> 
-    <!-- 添加属性传参可修改Css样式 -->
-    <router-link to="/login">登录页</router-link>
-  </el-row>
+  <el-container class="wrapper">
+    <el-header>Hacker News</el-header>
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -14,10 +17,46 @@ export default {
 
 <style lang="less" scoped>
 .wrapper{
+    // 容器占满窗口高度
     position: absolute;
     width: 100%;
-    top: 0;
-    bottom: 0;
+    height: 100%;
 
 }
+  .el-header {
+    background-color: #1e1e2f;
+    color: #ffffff;
+    text-align: left;
+    padding-left: 24px;
+    line-height: 60px;
+  }
+  
+  .el-aside {
+    background-color: #e04ecb;
+    color: #ffffff;
+    text-align: center;
+    line-height: 200px;
+    border-radius: 8px;
+    margin-top: 10px;
+  }
+  
+  .el-main {
+    // background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+  
+  body > .el-container {
+    margin-bottom: 40px;
+  }
+  
+  .el-container:nth-child(5) .el-aside,
+  .el-container:nth-child(6) .el-aside {
+    line-height: 260px;
+  }
+  
+  .el-container:nth-child(7) .el-aside {
+    line-height: 320px;
+  }
 </style>
