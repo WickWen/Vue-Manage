7878dc1806687a3973492927c84f17ff79fa3ebb
WickWen <mrwickweb@163.com>
2020-02-29 02:58:22 +0800

    使用照片墙组件并配置属性

--- src/views/EditPage.vue
+++ src/views/EditPage.vue
@@ -1,11 +1,45 @@
 <template>
   <div>
-      编辑文章
+    <el-upload
+      :action="$axios.defaults.baseURL + '/upload'" 
+      :headers="{Authorization:token}"
+      list-type="picture-card"
+      :on-success="handleSuccess"
+      :on-remove="handleRemove"
+      :on-preview="handlePreview">
+      <!-- 组件封装了ajax请求 axios设置就无效了 -->
+      <!-- 注意这里是v-bind : 而不是v-on @ -->
+      <i class="el-icon-plus"></i>
+    </el-upload>
+
+    <!-- 预览图片功能 -->
+    <el-dialog :visible.sync="dialogVisible">
+      <img width="50%" :src="dialogImageUrl" alt="">
+    </el-dialog>
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      token: localStorage.getItem('token'),
+      dialogImageUrl: '',
+      dialogVisible: false
+    }
+  },
+  methods: {
+    handleSuccess(res,flie,flieList){
+      console.log(res.data.url);
+    },
+    handleRemove(flie,flieList){
+
+    },
+    handlePreview(file){
+      this.dialogImageUrl = file.url;
+      this.dialogVisible = true;
+    }
+  },
 
 }
 </script>
